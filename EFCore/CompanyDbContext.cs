eb79c5af19ee880a21683a93f05ed8be5ba3e5d5
islam helmy <islam7lmy@gmail.com>
2026-05-09 17:58:55 +0300

    efCore

---
+++ EFCore/CompanyDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore
+{
+    internal class CompanyDbContext : DbContext
+    {
+        //"Data Source=ISLAM7LMYLAPVM\\ISLAM7LMY;Initial Catalog=MyCompany;User ID=sa;Password=***********;Encrypt=False"
+        //"Data Source = ISLAM7LMY\\ISLAM7LMY; intial Catalog = S2E.EFMYCompany; Integrated Security = True"
+        //"Data Source = ISLAM7LMY\\ISLAM7LMY; intial Catalog = S2E.EFMYCompany; User Id = sa; Password = Islam@123"
+        //"Server = ISLAM7LMY\\ISLAM7LMY; Database = S2E.EFMYCompany; Trusted_Connection = True"
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = .; Database = DKH4_SWD5_S1.EFMYCompany; Trusted_Connection = True; Encrypt = False");
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
